26706782f48768c2d3d7b17d415e1d3579267e0d
Anna Lidia Błaszczyk <stuff@alblaszczyk.oib.com>
2017-11-05 21:31:09 +0100

    Finally fixed the issue with title not linking to posts.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 # Create your views here.
 
