8d6dd34406e638cbae05973b94cfb7542ffcca78
Anna Lidia Błaszczyk <stuff@alblaszczyk.oib.com>
2017-11-12 11:31:20 +0100

    Changed slugs, broke editing and adding new posts.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -7,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(max_length=140, unique=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +18,17 @@ class Post(models.Model):
         return self.title
     def __repr__(self):
         return self.title
+
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Post.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save()
