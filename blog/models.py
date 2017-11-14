50243f7e946995f5514c704bf8a26f8cd6385cd0
Anna Lidia Błaszczyk <1437346+alblaszczyk@users.noreply.github.com>
2017-11-14 02:06:29 +0100

    Fix counting comments.

--- blog/models.py
+++ blog/models.py
@@ -33,6 +33,9 @@ class Post(models.Model):
             self.slug = self._get_unique_slug()
         super().save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
@@ -40,12 +43,9 @@ class Comment(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.text
-
     def approve(self):
         self.approved_comment = True
         self.save()
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
+    def __str__(self):
+        return self.text
